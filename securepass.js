cd0f4c48fb246a345c3ce6671461e781ea074832
1asalinas <42749574+1asalinas@users.noreply.github.com>
2018-12-11 13:57:15 -0600

    Add files via upload

--- securepass.js
+++ securepass.js
@@ -7,112 +7,90 @@ function lengthCheck(password){
   else{
     return false;
   }
-
 }
-
-
-
-
-
 
 //returns true or false if there is an uppercase character in the password
 function upperCaseCheck(password){
 
   for(let i = 0; i <= password.length; i++){
 
-    if(password.fromCharCode(i) >= 65 && password.fromCharCode(i) <= 90){
+    if(password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90){
       return true;
     }
-
   }
   return false;
 }
-
-
-
-
-
 
 //returns true or false if there is an lowercase character in the password
 function lowerCaseCheck(password){
 
   for(let i = 0; i <= password.length; i++){
 
-    if(password.fromCharCode(i) >= 97 && password.fromCharCode(i) <= 122){
+    if(password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122){
       return true;
     }
-
   }
   return false;
 }
-
-
-
-
-
-
-
 
 //returns true or false if there is a numerical character
 function numberCheck(password){
 
   for(let i = 0; i <= password.length; i++){
 
-    if(password.fromCharCode(i) >= 48 && password.fromCharCode(i) <= 57){
+    if(password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57){
       return true;
     }
-
   }
   return false;
-
 }
-
-
-
-
-
-
 
 //returns true or false if there is a special character in the password
 function specialCheck(check){
 
   for(let i = 0; i <= password.length; i++){
 
-    if(password.fromCharCode(i) >= 33 && password.fromCharCode(i) <= 47 ||
-    password.fromCharCode(i) >= 58 && password.fromCharCode(i) <= 64 ||
-    password.fromCharCode(i) >= 91 && password.fromCharCode(i) <= 96 ||
-    password.fromCharCode(i) >= 123 && password.fromCharCode(i) <= 126){
+    if(password.charCodeAt(i) >= 33 && password.charCodeAt(i) <= 47 || password.charCodeAt(i) >= 58 && password.charCodeAt(i) <= 64 || password.charCodeAt(i) >= 91 && password.charCodeAt(i) <= 96 || password.charCodeAt(i) >= 123 && password.charCodeAt(i) <= 126){
       return true;
     }
-
   }
   return false;
-
 }
 
-
-
-
-
-
-
-
-
-
-function passedAllChecks(){
-
+function passedAllChecks(password){
+  return lengthCheck(password) && upperCaseCheck(password) && lowerCaseCheck(password) && numberCheck(password) && specialCheck(password);
 }
 
+function superLengthCheck(password){
 
-
-
-
-
-
+  if(password.length >= 20){
+    return true
+  }
+  else{
+    return false;
+  }
+}
 
 
 //returns a string which represents the grade for your password
 function grade(password){
-
-
+  
+  if(!lengthCheck(password)){
+  return "very bad"
+  }
+  else if(!upperCaseCheck(password) || !lowerCaseCheck(password)){
+  return "bad"
+  }
+  else if(!numberCheck(password)){
+  return "okay"
+  }
+  else if(!specialCheck(check)){
+  return "good"
+  }
+  else if(passedAllChecks(password)){
+  return "great"
+  }
+  else if(passedAllChecks(password) && superLengthCheck(password)){
+  return "super!"
+  }
 }
